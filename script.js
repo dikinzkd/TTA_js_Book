6fe93ff851b25050489a2070d9ea079b368245cb
dikinzkd <dikinzkamadvd@gmail.com>
2024-10-19 09:10:04 +0200

    feat:completed the layout

--- script.js
+++ script.js
@@ -13,6 +13,30 @@ const recipes =[
         title: "Vegetable Stir-fry",
         ingredients: "Broccoli, Carrots, Bell Peppers, Soy Sauce, Garlic, Olive Oil",
         steps: "1. Stir-fry vegetables in olive oil. 2. Add garlic and soy sauce. 3. Serve with rice."
-    }
-
+    },
+    // {
+    //     title: "Pancakes",
+    //     ingredients: "Flour, Eggs, Milk, Sugar, Baking Powder, Salt",
+    //     steps: "1. Mix ingredients in a bowl. 2. Pour batter on a hot griddle. 3. Flip pancake when bubbles form."
+    // },
 ];
+
+const dispalyRecipes = () =>{
+    const recipelist = document.querySelector("#recipelist");
+    recipelist.innerHTML ="";
+
+    recipes.forEach((recipe) =>{
+        const recipeCard = document.createElement("div");
+        recipeCard.classList.add("bg-white", "p-4","rounded", "shadow", "m-4");
+
+        recipeCard.innerHTML =`
+        <h2  class="text-lg font-bold">${recipe.title}</h2>
+        <p class="text-sm text-gray-400"><strong>Ingredients: &emap;<strong/>${recipe.ingredients}</p>
+        <p  class="text-sm"><strong>Steps: &emap;<strong/>${recipe.steps}</p>
+        `;
+        recipelist.appendChild(recipeCard);
+    }
+)
+}
+
+dispalyRecipes();
